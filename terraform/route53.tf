f5720d44733c719b7979b7e8d0fe1fef7363e5df
Cody Lee <cbricel20@gmail.com>
2025-02-26 22:46:33 -0600

    Updating route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,10 +1,14 @@
-# resource "aws_route53_zone" "main" {
+resource "aws_route53_zone" "main" {
+    name = "codyleetech.com"
+}
 
-# }
-
-# resource "aws_route53_record" "web_a_rec" {
-  
-# }
+resource "aws_route53_record" "web_a_rec" {
+    zone_id = aws_route53_zone.main.zone_id
+    name = "codyleetech.com"
+    type = "A"
+    ttl = "300"
+    records = ["${aws_cloudfront_distribution.web_cdn.domain_name}"]
+}
 
 # resource "aws_route53_record" "www_web_a_rec" {
   
